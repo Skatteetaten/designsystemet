7e524aa4407a5c96fa0c553c907a4fe139e9fa84
Johannes Berg <Johannes.Berg@skatteetaten.no>
2022-08-18 13:50:02 +0200

    setter opp image snapshot testing med storybook og puppeteer

--- apps/designsystem-playground/storybook.test.ts
+++ apps/designsystem-playground/storybook.test.ts
@@ -1,9 +1,12 @@
 import initStoryshots from '@storybook/addon-storyshots';
+import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 import { render } from '@testing-library/react';
 
 import { resolve } from 'path';
 
 initStoryshots({
+  suite: 'Image storyshots',
+  test: imageSnapshot({ storybookUrl: 'http://localhost:4400' }),
   configPath: resolve(__dirname, './.storybook'),
   framework: 'react',
   renderer: render,
