67f5c206099c5b41add205cad0d2fee1fb79442a
Johannes Berg <Johannes.Berg@skatteetaten.no>
2022-08-24 08:26:22 +0200

    Fiks type

--- libs/ds-buttons/src/lib/ActionButton/index.tsx
+++ libs/ds-buttons/src/lib/ActionButton/index.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 import styles from './ActionButton.module.scss';
 
-export const ActionButton: FC = (): any => {
+export const ActionButton: FC = (): JSX.Element => {
   return (
     <div>
       <button className={styles['actionButton']}>{'Action!'}</button>
