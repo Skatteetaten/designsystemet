dc7a3ded8eadd7f01b2425f81757b5daff76a30f
Johannes Berg <Johannes.Berg@skatteetaten.no>
2022-08-25 13:39:37 +0200

    styles['klasse'] til styles.klasse

--- libs/ds-buttons/src/lib/ActionButton/index.tsx
+++ libs/ds-buttons/src/lib/ActionButton/index.tsx
@@ -5,7 +5,7 @@ import styles from './ActionButton.module.scss';
 export const ActionButton: FC = (): JSX.Element => {
   return (
     <div>
-      <button className={styles['actionButton']}>{'Action!'}</button>
+      <button className={styles.actionButton}>{'Action!'}</button>
     </div>
   );
 };
