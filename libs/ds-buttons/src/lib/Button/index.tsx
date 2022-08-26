b46eab02612f220436c581c591422a921df0b34f
Johannes Berg <Johannes.Berg@skatteetaten.no>
2022-08-26 13:03:48 +0200

    rettelser etter code review.

--- libs/ds-buttons/src/lib/Button/index.tsx
+++ libs/ds-buttons/src/lib/Button/index.tsx
@@ -2,7 +2,7 @@ import styles from './button.module.scss';
 
 export function Button(): JSX.Element {
   return (
-    <div className={styles.button}>
+    <div className={styles.test}>
       <h1>{'Welcome to awesome Button!'}</h1>
     </div>
   );
