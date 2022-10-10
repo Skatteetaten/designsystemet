05d409a4a08cec8c02b7f8db70fb8b42f7fe4f18
Cecilie Nordbø <Cecilie.Nordbo@skatteetaten.no>
2022-10-10 15:26:09 +0200

    FRONT-893, FRONT-937 og FRONT-929: ny knapp InlineButton

---
+++ apps/ds-playground/src/stories/InlineButton.stories.tsx
@@ -0,0 +1,90 @@
+import {
+  InlineButton,
+  getDisabledDefault,
+  getPositionDefault,
+} from '@skatteetaten/ds-buttons';
+import {
+  AddOutlineSVGpath,
+  BellSVGpath,
+  ChatbotSVGpath,
+} from '@skatteetaten/ds-icons';
+import { action } from '@storybook/addon-actions';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+// TODO FRONT-893 - legge på figma-lenker
+
+// TODO FRONT-893 - komplett list med ikoner
+const iconList = {
+  AddOutline: { svgPath: AddOutlineSVGpath },
+  Bell: { svgPath: BellSVGpath },
+  Chatbot: { svgPath: ChatbotSVGpath },
+};
+
+export default {
+  component: InlineButton,
+  title: 'Design System / InlineButton',
+  argTypes: {
+    id: {
+      control: 'text',
+      table: { defaultValue: { summary: '' } },
+    },
+    children: {
+      control: 'text',
+      table: { defaultValue: { summary: '' } },
+    },
+    className: {
+      control: 'select',
+      options: ['', 'buttonClassnameDark', 'buttonClassnameLight'],
+      table: { defaultValue: { summary: '' } },
+    },
+    disabled: {
+      control: 'boolean',
+      table: { defaultValue: { summary: getDisabledDefault() } },
+    },
+    svgPath: {
+      options: [''].concat(Object.keys(iconList)),
+      control: 'select',
+      mapping: iconList,
+    },
+    iconPosition: {
+      control: 'select',
+      options: ['left', 'right'],
+      table: { defaultValue: { summary: getPositionDefault() } },
+    },
+    'aria-describedby': {
+      control: 'text',
+      table: { defaultValue: { summary: '' } },
+    },
+    accessKey: {
+      control: 'text',
+      table: { defaultValue: { summary: '' } },
+    },
+    tabIndex: {
+      control: 'text',
+      table: { defaultValue: { summary: '' } },
+    },
+  },
+} as ComponentMeta<typeof InlineButton>;
+
+const TemplateDefault: ComponentStory<typeof InlineButton> = (args) => (
+  <InlineButton {...args} onClick={action('klikkEvent InlineButton')}>
+    {args.children}
+  </InlineButton>
+);
+
+const baseArgs = {
+  children: 'Legg til rapport',
+};
+
+export const InlineButtonDefault = TemplateDefault.bind({});
+InlineButtonDefault.storyName = 'Default';
+InlineButtonDefault.args = {
+  ...baseArgs,
+};
+
+export const InlineButtonIcon = TemplateDefault.bind({});
+InlineButtonIcon.storyName = 'With icon';
+InlineButtonIcon.args = {
+  ...baseArgs,
+  svgPath: AddOutlineSVGpath,
+};
