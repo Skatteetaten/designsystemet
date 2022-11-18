1f916dfa2e6099e73a46ac8777852e4e1fc88076
Johannes Berg <Johannes.Berg@skatteetaten.no>
2022-11-18 13:42:20 +0100

    justering på size for IconButton

--- libs/ds-buttons/src/IconButton/IconButton.types.ts
+++ libs/ds-buttons/src/IconButton/IconButton.types.ts
@@ -22,7 +22,7 @@ export interface IconButtonProps
   /** Om knappen har en ramme/sirkel. */
   isOutlined?: boolean;
   /** Størrelse på knappen. */
-  size?: Extract<Size, 'small' | 'medium' | 'large'>;
+  size?: Extract<Size, 'extraSmall' | 'small' | 'medium' | 'large'>;
   /** HTML-path node. Forhåndsdefinerte paths kan importeres fra @skatteetaten/ds-icons pakke. Alternativt kan custom path sendes. */
   svgPath: ReactElement;
 }
