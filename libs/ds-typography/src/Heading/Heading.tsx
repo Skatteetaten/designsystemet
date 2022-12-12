51d5f319062c876a4fe8a356af2f841c1083448d
Ann Magdalen Hegge <annmagdalen.hegge@skatteetaten.no>
2022-12-12 12:31:13 +0100

    Opprettet Heading komponent

---
+++ libs/ds-typography/src/Heading/Heading.tsx
@@ -0,0 +1,51 @@
+import { forwardRef } from 'react';
+
+import { getCommonClassNameDefault } from '@skatteetaten/ds-core-utils';
+
+import { HeadingProps } from './Heading.types';
+
+import styles from './Heading.module.scss';
+
+export const Heading = forwardRef<HTMLHeadingElement, HeadingProps>(
+  (
+    {
+      id,
+      className = getCommonClassNameDefault(),
+      lang,
+      'data-testid': dataTestId,
+      as: Tag,
+      level,
+      hasSpacing,
+      children,
+    },
+    ref
+  ): JSX.Element => {
+    const levelClassName = level ? styles[`heading_level${level}`] : '';
+    let spacingClassName = '';
+    if (hasSpacing) {
+      if (level === 1) {
+        spacingClassName = styles[`heading_hasSpacingMedium`];
+      } else if (level === 2) {
+        spacingClassName =
+          styles[`heading_hasSpacingSmallMobileMediumOtherwise`];
+      } else if (level === 3 || level === 4) {
+        spacingClassName = styles[`heading_hasSpacingSmall`];
+      } else if (level === 5) {
+        spacingClassName = styles[`heading_hasSpacingExtraSmall`];
+      }
+    }
+    return (
+      <Tag
+        ref={ref}
+        id={id}
+        className={`${styles.heading} ${levelClassName} ${spacingClassName} ${className}`}
+        lang={lang}
+        data-testid={dataTestId}
+      >
+        {children}
+      </Tag>
+    );
+  }
+);
+
+Heading.displayName = 'Heading';
--- libs/ds-typography/src/index.ts
+++ libs/ds-typography/src/index.ts
@@ -1,4 +1,6 @@
 export * from './Blockquote/Blockquote';
 export * from './Blockquote/Blockquote.types';
+export * from './Heading/Heading';
+export * from './Heading/Heading.types';
 export * from './Paragraph/Paragraph';
 export * from './Paragraph/Paragraph.types';
