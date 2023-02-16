7cc24fa1a79877edf25fab46d3b06239795e4a5e
Hegge, Ann Magdalen <AnnMagdalen.Hegge@skatteetaten.no>
2023-02-16 09:27:47 +0100

    Pull request #122: FRONT-1076: Add theme to Storybook
    Merge in AUF/designsystemet from feature/FRONT-1076-skatteetaten-tema-i-storybook to develop
    
    Squashed commit of the following:
    
    commit 77f4c5f34403557f2b3210ca97c046110b637a5b
    Author: Ann Magdalen Hegge <annmagdalen.hegge@skatteetaten.no>
    Date:   Thu Feb 9 14:20:51 2023 +0100
    
        Add theme to Storybook

---
+++ apps/ds-playground/.storybook/theme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  appBg: '#f4f4f4', // graphite-5
+  colorPrimary: '#1362ae', // azure-70
+  colorSecondary: '#1362ae', // azure-70
+  brandImage:
+    'https://www.skatteetaten.no/globalassets/design/logo/skatteetaten/skjerm/horisontal/skatteetaten-logo-horisontal-rgb-svart.png',
+});
