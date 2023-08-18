ea8ffee7267e6bc12d071f36c89af3f18a0d8212
Ann Magdalen Hegge <annmagdalen.hegge@skatteetaten.no>
2023-08-18 12:39:40 +0200

    Add package ds-overlays

---
+++ libs/ds-overlays/src/Popover/Popover.tsx
@@ -0,0 +1,32 @@
+import { forwardRef } from 'react';
+
+import { getCommonClassNameDefault } from '@skatteetaten/ds-core-utils';
+
+import { PopoverProps } from './Popover.types';
+
+export const Popover = forwardRef<HTMLDivElement, PopoverProps>(
+  (
+    {
+      id,
+      className = getCommonClassNameDefault(),
+      lang,
+      'data-testid': dataTestId,
+      children,
+    },
+    ref
+  ): JSX.Element => {
+    return (
+      <div
+        ref={ref}
+        id={id}
+        className={className}
+        lang={lang}
+        data-testid={dataTestId}
+      >
+        {children}
+      </div>
+    );
+  }
+);
+
+Popover.displayName = 'Popover';
