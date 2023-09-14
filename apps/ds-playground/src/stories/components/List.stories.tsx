04c3e5e0b36bf3155893f1ba76fd5f722bef5a6d
Olav Hellesø-Knutsen <olav.helleso-knutsen@skatteetaten.no>
2023-09-14 10:53:35 +0200

    Flyttet children prop til riktig kategori i storybook

--- apps/ds-playground/src/stories/components/List.stories.tsx
+++ apps/ds-playground/src/stories/components/List.stories.tsx
@@ -31,6 +31,10 @@ const meta = {
         defaultValue: { summary: getListAsDefault() },
       },
     },
+    children: {
+      control: 'text',
+      table: { category: category.props },
+    },
   },
   tags: ['autodocs'],
   parameters: {
