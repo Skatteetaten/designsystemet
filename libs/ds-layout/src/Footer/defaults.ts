55462a31feac7aa3cfa434db0b3db99217ffb726
Ann Magdalen Hegge <annmagdalen.hegge@skatteetaten.no>
2023-09-15 11:14:10 +0200

    Remove default urls Footer links

--- libs/ds-layout/src/Footer/defaults.ts
+++
@@ -1,5 +0,0 @@
-export const getContactLinkDefault = (): string =>
-  'https://www.skatteetaten.no/kontakt/';
-export const getSecurityLinkDefault = (): string =>
-  'https://www.skatteetaten.no/om-skatteetaten/sikkerhet/';
-export const getAccessibilityLinkDefault = (): string => 'https://uustatus.no/';
