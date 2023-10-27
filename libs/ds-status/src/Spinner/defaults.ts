209d4ffc9c1d7e709e8b1691d7933aae435a06f5
Johannes Berg <Johannes.Berg@skatteetaten.no>
2023-10-27 15:22:08 +0200

    FRONT-1363: Opprettet komponent Spinner

---
+++ libs/ds-status/src/Spinner/defaults.ts
@@ -0,0 +1,7 @@
+//export const getSpinnerColorDefault = () =>
+import { SpinnerColor, SpinnerTextPostion, SpinnerSize } from './Spinner.types';
+
+export const getSpinnerTextPositionDefault = (): SpinnerTextPostion => 'bottom';
+
+export const getSpinnerSizeDefault = (): SpinnerSize => 'medium';
+export const getSpinnerColorDefault = (): SpinnerColor => 'black';
