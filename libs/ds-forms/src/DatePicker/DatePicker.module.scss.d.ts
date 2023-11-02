729458e0e82b348cd9b1b4c9dce067aeb0226e53
Cecilie Tm9yZGLDuA== <Cecilie.Nordbo@skatteetaten.no>
2023-11-02 21:37:21 +0100

    FRONT-1346: Begynt med DatePicker-komponenten sin dato-felt og kalenderknapp inkl. tester, noen småting gjenstår

---
+++ libs/ds-forms/src/DatePicker/DatePicker.module.scss.d.ts
@@ -0,0 +1,8 @@
+/* automatisk genererte types */
+export const date_container: string;
+export const input: string;
+export const input_large: string;
+export const calendarButton: string;
+export const icon: string;
+export const calendarButton_large: string;
+export const calendar_container: string;
