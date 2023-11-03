a19a0397f0acb56e8b7374743590126578f12ffd
Johannes Berg <Johannes.Berg@skatteetaten.no>
2023-11-03 12:29:44 +0100

    generer riktig lintFilePatterns i nye libs

--- tools/workspace-plugin/src/generators/ds-library/index.ts
+++ tools/workspace-plugin/src/generators/ds-library/index.ts
@@ -142,6 +142,7 @@ export default async function (
     return packageJson;
   });
 
+  const lint = projectConfig?.targets?.lint;
   const projectConfigWithRollupOptions = {
     ...projectConfig,
     implicitDependencies: ['!ds-dev-config'],
@@ -152,6 +153,16 @@ export default async function (
         options: {
           ...projectConfig?.targets.build.options,
           rollupConfig: [`libs/${projectName}/rollup.config.js`],
+        },
+      },
+      lint: {
+        ...lint,
+        options: {
+          ...lint.options,
+          lintFilePatterns: [
+            ...lint.options.lintFilePatterns,
+            `libs/${projectName}/package.json`,
+          ],
         },
       },
     },
