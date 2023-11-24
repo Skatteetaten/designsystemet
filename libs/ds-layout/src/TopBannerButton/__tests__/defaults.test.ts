9b58bf885bc86eb43b0bd63eaddee89c7a1de3ad
Ann Magdalen Hegge <annmagdalen.hegge@skatteetaten.no>
2023-11-24 10:09:00 +0100

    Refactor css TopBannerButton

--- libs/ds-layout/src/TopBannerButton/__tests__/defaults.test.ts
+++
@@ -1,8 +0,0 @@
-import { getTopBannerButtonVariantDefault } from '../defaults';
-
-describe('TopBannerButton defaults', () => {
-  it('Når getTopBannerButtonVariantDefault kalles, så returnerer den riktig verdi', () => {
-    const renderDefault = getTopBannerButtonVariantDefault();
-    expect(renderDefault).toBe('standard');
-  });
-});
