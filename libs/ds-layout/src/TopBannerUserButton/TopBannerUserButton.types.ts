3ab5e0f3db62b11d7abc2afb536c17273d54050e
Cecilie Tm9yZGLDuA== <Cecilie.Nordbo@skatteetaten.no>
2023-11-28 11:19:11 +0100

    FRONT-1161 TopBanner - endret tilbake css, fjernet esc pga mainMenu skal ikke ha overlay og annet smått

--- libs/ds-layout/src/TopBannerUserButton/TopBannerUserButton.types.ts
+++ libs/ds-layout/src/TopBannerUserButton/TopBannerUserButton.types.ts
@@ -7,7 +7,7 @@ export interface TopBannerUserButtonProps extends BaseProps {
   /** Teksten på knappen. Hva teksten skal være henger sammen med userRole. Når rollen er 'meg', settes brukernavn til 'Meg selv' automatisk. Hvis rollen er 'andre', må det settes brukernavn til navnet på person det gjelder mens rollen 'virksomhet' settes navnet på virksomheten. */
   children?: string;
   /** Hvilken rolle innlogget bruker har. */
-  role: UserRole;
+  role?: UserRole;
   /** Callback når knappen trykkes på. */
   onUsername?: () => void;
 }
