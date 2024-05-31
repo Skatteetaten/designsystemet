0083ced8d8c7edb8b8abdc5d968aa4eb6d36a7ff
Berg, Johannes <Johannes.Berg@skatteetaten.no>
2024-05-31 12:52:36 +0200

    Applied suggestion

--- libs/ds-collections/src/TabsTab/TabsTab.types.ts
+++ libs/ds-collections/src/TabsTab/TabsTab.types.ts
@@ -5,7 +5,7 @@ import { BaseProps } from '@skatteetaten/ds-core-utils';
 export interface TabsTabProps extends BaseProps {
   /** Innhold i en tab */
   children: string;
-  /** ssHTML-path node. Forhåndsdefinerte paths kan importeres fra ds-icons pakke. Alternativt kan custom path sendes. */
+  /** HTML-path node. Forhåndsdefinerte paths kan importeres fra ds-icons pakke. Alternativt kan custom path sendes. */
   svgPath?: ReactElement;
   /** Verdien som lenker sammen Tabs.Tab og Tabs.Panel sammen */
   value: string;
