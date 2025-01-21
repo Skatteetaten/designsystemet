65e08bf5320b56e68822eddb1263c8b4f37723c0
Jørgen Høivik Bye <jorgenhoivik.bye@skatteetaten.no>
2025-01-21 10:32:31 +0100

    fjern autogenerert fil som hang igjen etter typofix + snapshots

--- libs/ds-overlays/src/RolePicker/RolPicker.module.scss.d.ts
+++
@@ -1,8 +0,0 @@
-declare const classNames: {
-  readonly rolePicker: 'rolePicker';
-  readonly rolePickerFooter: 'rolePickerFooter';
-  readonly container: 'container';
-  readonly showMoreButton: 'showMoreButton';
-  readonly srOnly: 'srOnly';
-};
-export default classNames;
