e3e540f551278644f8a7e280c342c95fdcaed348
Hegge <annmagdalen.hegge@skatteetaten.no>
2025-03-14 09:46:37 +0100

    Update presentation of container tokens

--- apps/ds-playground/src/stories/designtokens/containers.stories.tsx
+++ apps/ds-playground/src/stories/designtokens/containers.stories.tsx
@@ -1,13 +1,163 @@
 import { StoryObj } from '@storybook/react';
 
-import { TokensList } from './TokensList';
+import { Table } from '@skatteetaten/ds-table';
+import { Paragraph } from '@skatteetaten/ds-typography';
 
 export default {
   title: 'Designtokens/Containers',
 };
 
 export const Containers: StoryObj = {
-  render: () => <TokensList tokenType={'containers'} />,
+  render: () => (
+    <>
+      <ul>
+        <li>
+          <span className={'designtokenName'}>{'--container-xs: '}</span>
+          {'100%'}
+        </li>
+        <li>
+          <span className={'designtokenName'}>{'--container-s: '}</span>
+          {'560px'}
+        </li>
+        <li>
+          <span className={'designtokenName'}>{'--container-m: '}</span>
+          {'960px'}
+        </li>
+        <li>
+          <span className={'designtokenName'}>{'--container-l: '}</span>
+          {'1160px'}
+        </li>
+        <li>
+          <span className={'designtokenName'}>{'--container-aside: '}</span>
+          {'380px'}
+        </li>
+        <li>
+          <span className={'designtokenName'}>
+            {'--container-wide-content: '}
+          </span>
+          {'760px'}
+        </li>
+      </ul>
+      <Paragraph>
+        {'Når dynamiske container-tokens endres fra utgangspunktet (mobile):'}
+      </Paragraph>
+      <Table
+        caption={
+          'Når dynamiske container-tokens endres fra utgangspunktet (mobile)'
+        }
+        variant={'compact'}
+      >
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell colSpan={2}>{''}</Table.HeaderCell>
+            <Table.HeaderCell>{'Mobile'}</Table.HeaderCell>
+            <Table.HeaderCell>{'Breakpoint XS'}</Table.HeaderCell>
+            <Table.HeaderCell>{'Breakpoint S'}</Table.HeaderCell>
+            <Table.HeaderCell>{'Breakpoint M'}</Table.HeaderCell>
+            <Table.HeaderCell>{'Breakpoint L'}</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          <Table.Row>
+            <Table.DataCell rowSpan={3} className={'rowRightBorder'}>
+              <strong>{'External'}</strong>
+            </Table.DataCell>
+            <Table.DataCell className={'rowRightBorder'}>
+              <strong>{'--semantic-responsive-container'}</strong>
+            </Table.DataCell>
+            <Table.DataCell colSpan={2} className={'rowRightBorder'}>
+              {'var(--container-xs)'}
+            </Table.DataCell>
+            <Table.DataCell className={'rowRightBorder'}>
+              {'var(--container-s)'}
+            </Table.DataCell>
+            <Table.DataCell className={'rowRightBorder'}>
+              {'var(--container-m)'}
+            </Table.DataCell>
+            <Table.DataCell className={'rowRightBorder'}>
+              {'var(--container-l)'}
+            </Table.DataCell>
+          </Table.Row>
+          <Table.Row>
+            <Table.DataCell className={'rowRightBorder'}>
+              <strong>{'--semantic-responsive-container-padding'}</strong>
+            </Table.DataCell>
+            <Table.DataCell colSpan={2} className={'rowRightBorder'}>
+              {'0 var(--spacing-m)'}
+            </Table.DataCell>
+            <Table.DataCell colSpan={3} className={'rowRightBorder'}>
+              {'0'}
+            </Table.DataCell>
+          </Table.Row>
+          <Table.Row>
+            <Table.DataCell className={'rowRightBorder'}>
+              <strong>{'--semantic-responsive-article'}</strong>
+            </Table.DataCell>
+            <Table.DataCell colSpan={2} className={'rowRightBorder'}>
+              {'var(--container-xs)'}
+            </Table.DataCell>
+            <Table.DataCell colSpan={3} className={'rowRightBorder'}>
+              {'var(--container-s)'}
+            </Table.DataCell>
+          </Table.Row>
+          <Table.Row>
+            <Table.DataCell rowSpan={4} className={'rowRightBorder'}>
+              <strong>{'Internal'}</strong>
+            </Table.DataCell>
+            <Table.DataCell className={'rowRightBorder'}>
+              <strong>
+                {'--semantic-responsive-internal-container-display'}
+              </strong>
+            </Table.DataCell>
+            <Table.DataCell colSpan={3} className={'rowRightBorder'}>
+              {'block'}
+            </Table.DataCell>
+            <Table.DataCell colSpan={2} className={'rowRightBorder'}>
+              {'grid'}
+            </Table.DataCell>
+          </Table.Row>
+          <Table.Row>
+            <Table.DataCell className={'rowRightBorder'}>
+              <strong>
+                {'--semantic-responsive-internal-container-padding'}
+              </strong>
+            </Table.DataCell>
+            <Table.DataCell colSpan={3} className={'rowRightBorder'}>
+              {'0 var(--spacing-m)'}
+            </Table.DataCell>
+            <Table.DataCell colSpan={2} className={'rowRightBorder'}>
+              {'0 var(--spacing-xl)'}
+            </Table.DataCell>
+          </Table.Row>
+          <Table.Row>
+            <Table.DataCell className={'rowRightBorder'}>
+              <strong>{'--semantic-responsive-internal-aside'}</strong>
+            </Table.DataCell>
+            <Table.DataCell colSpan={3} className={'rowRightBorder'}>
+              {'var(--container-xs)'}
+            </Table.DataCell>
+            <Table.DataCell colSpan={2} className={'rowRightBorder'}>
+              {'var(--container-aside)'}
+            </Table.DataCell>
+          </Table.Row>
+          <Table.Row>
+            <Table.DataCell className={'rowRightBorder'}>
+              <strong>{'--semantic-responsive-wide-content'}</strong>
+            </Table.DataCell>
+            <Table.DataCell className={'rowRightBorder'}>
+              {'var(--container-xs)'}
+            </Table.DataCell>
+            <Table.DataCell className={'rowRightBorder'}>
+              {'var(--container-s)'}
+            </Table.DataCell>
+            <Table.DataCell colSpan={3} className={'rowRightBorder'}>
+              {'var(--container-wide-content)'}
+            </Table.DataCell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    </>
+  ),
   parameters: {
     previewTabs: {
       canvas: {
