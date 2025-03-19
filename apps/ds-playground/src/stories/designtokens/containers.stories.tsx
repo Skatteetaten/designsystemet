9c5038ec21328181ad9cb84c7526b391848c2238
Hegge <annmagdalen.hegge@skatteetaten.no>
2025-03-19 13:02:52 +0100

    Keep hardcoded values in Table

--- apps/ds-playground/src/stories/designtokens/containers.stories.tsx
+++ apps/ds-playground/src/stories/designtokens/containers.stories.tsx
@@ -1,6 +1,5 @@
 import { StoryObj } from '@storybook/react';
 
-import containersJson from '@skatteetaten/ds-core-designtokens/designtokens/containers.json';
 import { Table } from '@skatteetaten/ds-table';
 import { Paragraph } from '@skatteetaten/ds-typography';
 
@@ -10,60 +9,8 @@ export default {
   title: 'Designtokens/Containers',
 };
 
-interface Breakpoint {
-  name: string;
-  query: string;
-}
-
-const rootQuery = ':root,\n  :host';
-const breakpoints: Breakpoint[] = [
-  { name: 'Breakpoint XS', query: '@media (width >= 480px)' },
-  { name: 'Breakpoint S', query: '@media (width >= 640px)' },
-  { name: 'Breakpoint M', query: '@media (width >= 1024px)' },
-  { name: 'Breakpoint L', query: '@media (width >= 1366px)' },
-];
-
-const containersJsonTyped: any = containersJson;
-
-const generateTableRows = (
-  tokens: Record<string, string>,
-  breakpoints: Breakpoint[],
-  category: string,
-  start: number,
-  end: number
-): JSX.Element[] => {
-  return Object.keys(tokens)
-    .map((key, index) => (
-      <Table.Row key={key}>
-        {index === start && (
-          <Table.DataCell rowSpan={Math.floor(end - start)}>
-            <strong>{category}</strong>
-          </Table.DataCell>
-        )}
-        <Table.DataCell className={'rowLeftBorder'}>
-          <strong>{key}</strong>
-        </Table.DataCell>
-        <Table.DataCell className={'rowLeftBorder'}>
-          {tokens[key]}
-        </Table.DataCell>
-        {breakpoints.map((breakpoint) => (
-          <Table.DataCell
-            key={breakpoint.name}
-            className={`${containersJsonTyped[breakpoint.query][rootQuery][key] ? 'rowLeftBorder' : ''} ${breakpoint.name === 'Breakpoint L' ? 'rowRightBorder' : ''}`}
-          >
-            {containersJsonTyped[breakpoint.query][rootQuery][key] || ''}
-          </Table.DataCell>
-        ))}
-      </Table.Row>
-    ))
-    .slice(start, end);
-};
-
 export const Containers: StoryObj = {
   render: () => {
-    const responsiveTokens = Object.fromEntries(
-      Object.entries(containersJson[':root,\n:host']).slice(8, 16)
-    );
     return (
       <>
         <TokensList tokenType={'containers'} />
@@ -71,28 +18,6 @@ export const Containers: StoryObj = {
           {'Når dynamiske container-tokens endres fra utgangspunktet (mobile):'}
         </Paragraph>
         <Table
-          caption={
-            'Når dynamiske container-tokens endres fra utgangspunktet (mobile)'
-          }
-          variant={'compact'}
-        >
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell colSpan={2}>{''}</Table.HeaderCell>
-              <Table.HeaderCell>{'Mobile'}</Table.HeaderCell>
-              {breakpoints.map((breakpoint) => (
-                <Table.HeaderCell key={breakpoint.name} className={'noWrap'}>
-                  {breakpoint.name}
-                </Table.HeaderCell>
-              ))}
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {generateTableRows(responsiveTokens, breakpoints, 'External', 0, 3)}
-            {generateTableRows(responsiveTokens, breakpoints, 'Internal', 3, 8)}
-          </Table.Body>
-        </Table>
-        {/* <Table
           caption={
             'Når dynamiske container-tokens endres fra utgangspunktet (mobile)'
           }
@@ -221,7 +146,7 @@ export const Containers: StoryObj = {
               </Table.DataCell>
             </Table.Row>
           </Table.Body>
-        </Table> */}
+        </Table>
       </>
     );
   },
