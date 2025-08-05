0756daf7761656889ccc8b6ac3dfdc30499f8708
Johannes Berg <johannes.berg@skatteetaten.no>
2025-08-05 12:27:03 +0200

    Opprettet oppgaveliste story

---
+++ apps/ds-playground/src/stories/pages/Oppgaveliste.module.css.d.ts
@@ -0,0 +1,13 @@
+declare const classNames: {
+  readonly mainExternal: 'mainExternal';
+  readonly marginRightButton: 'marginRightButton';
+  readonly task: 'task';
+  readonly taskLink: 'taskLink';
+  readonly article: 'article';
+  readonly heading: 'heading';
+  readonly headingIcon: 'headingIcon';
+  readonly marginTopL: 'marginTopL';
+  readonly marginTopXl: 'marginTopXl';
+  readonly sendInButton: 'sendInButton';
+};
+export default classNames;
