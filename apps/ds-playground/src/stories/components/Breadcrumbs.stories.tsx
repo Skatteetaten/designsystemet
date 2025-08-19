1054b8124b4c4c4ed7f2bcfe239c84374e6ebb7f
Hegge <annmagdalen.hegge@skatteetaten.no>
2025-08-19 13:28:39 +0200

    Add external and internal examples Breadcrumbs

--- apps/ds-playground/src/stories/components/Breadcrumbs.stories.tsx
+++ apps/ds-playground/src/stories/components/Breadcrumbs.stories.tsx
@@ -6,6 +6,11 @@ import { Breadcrumbs, BreadcrumbsProps } from '@skatteetaten/ds-navigation';
 
 import { category } from '../../../.storybook/helpers';
 import { exampleParameters } from '../utils/stories.utils';
+import {
+  Footer,
+  TopBannerExternal,
+  TopBannerInternal,
+} from '@skatteetaten/ds-layout';
 
 Breadcrumbs.List.displayName = 'Breadcrumbs.List';
 Breadcrumbs.Item.displayName = 'Breadcrumbs.Item';
@@ -46,30 +51,77 @@ type Story = StoryObj<typeof meta>;
 
 export const Preview: Story = {} satisfies Story;
 
-export const Examples: Story = {
+export const ExternalExample: Story = {
+  name: 'I publikumsløsning',
   render: (_args): JSX.Element => {
     return (
-      <Breadcrumbs>
-        <Breadcrumbs.List shouldCollapse>
-          <Breadcrumbs.Item>
-            <Breadcrumbs.Link href={'#'}>
-              {'Bedrift og organisasjon'}
-            </Breadcrumbs.Link>
-          </Breadcrumbs.Item>
-          <Breadcrumbs.Item>
-            <Breadcrumbs.Link href={'#'}>{'Avgifter'}</Breadcrumbs.Link>
-          </Breadcrumbs.Item>
-          <Breadcrumbs.Item>
-            <Breadcrumbs.Link href={'#'}>
-              {'Avgifter på bil og kjøretøy'}
-            </Breadcrumbs.Link>
-          </Breadcrumbs.Item>
-          <Breadcrumbs.Item>
-            <Breadcrumbs.Link href={'#'}>{'Vektårsavgift'}</Breadcrumbs.Link>
-          </Breadcrumbs.Item>
-        </Breadcrumbs.List>
-      </Breadcrumbs>
+      <>
+        <TopBannerExternal />
+        {/* .breadcrumbs--external {
+          width: var(--semantic-responsive-container);
+          margin: var(--spacing-m) auto;
+          padding: var(--semantic-responsive-container-spacing);
+          box-sizing: border-box;
+        } */}
+        <Breadcrumbs className={'breadcrumbs--external'}>
+          <Breadcrumbs.List shouldCollapse>
+            <Breadcrumbs.Item>
+              <Breadcrumbs.Link href={'#'}>
+                {'Bedrift og organisasjon'}
+              </Breadcrumbs.Link>
+            </Breadcrumbs.Item>
+            <Breadcrumbs.Item>
+              <Breadcrumbs.Link href={'#'}>{'Avgifter'}</Breadcrumbs.Link>
+            </Breadcrumbs.Item>
+            <Breadcrumbs.Item>
+              <Breadcrumbs.Link href={'#'}>
+                {'Avgifter på bil og kjøretøy'}
+              </Breadcrumbs.Link>
+            </Breadcrumbs.Item>
+            <Breadcrumbs.Item>
+              <Breadcrumbs.Link href={'#'}>{'Vektårsavgift'}</Breadcrumbs.Link>
+            </Breadcrumbs.Item>
+          </Breadcrumbs.List>
+        </Breadcrumbs>
+      </>
     );
   },
 } satisfies Story;
-Examples.parameters = exampleParameters;
+ExternalExample.parameters = exampleParameters;
+ExternalExample.parameters = { layout: 'fullscreen' };
+
+export const InternalExample: Story = {
+  name: 'I intern løsning',
+  render: (_args): JSX.Element => {
+    return (
+      <>
+        <TopBannerInternal title={'Navn på system'} logoHref={'/'} />
+        {/* .breadcrumbs--internal {
+          padding: var(--spacing-s) var(--semantic-responsive-internal-container-spacing);
+        } */}
+        <Breadcrumbs className={'breadcrumbs--internal'}>
+          <Breadcrumbs.List shouldCollapse>
+            <Breadcrumbs.Item>
+              <Breadcrumbs.Link href={'#'}>
+                {'Bedrift og organisasjon'}
+              </Breadcrumbs.Link>
+            </Breadcrumbs.Item>
+            <Breadcrumbs.Item>
+              <Breadcrumbs.Link href={'#'}>{'Avgifter'}</Breadcrumbs.Link>
+            </Breadcrumbs.Item>
+            <Breadcrumbs.Item>
+              <Breadcrumbs.Link href={'#'}>
+                {'Avgifter på bil og kjøretøy'}
+              </Breadcrumbs.Link>
+            </Breadcrumbs.Item>
+            <Breadcrumbs.Item>
+              <Breadcrumbs.Link href={'#'}>{'Vektårsavgift'}</Breadcrumbs.Link>
+            </Breadcrumbs.Item>
+          </Breadcrumbs.List>
+        </Breadcrumbs>
+      </>
+    );
+  },
+} satisfies Story;
+InternalExample.parameters = exampleParameters;
+InternalExample.parameters = { layout: 'fullscreen' };
