4fd54dfd738d4bc822feadbb9595a09c3ffc7825
Johannes Berg <johannes.berg@skatteetaten.no>
2025-09-01 11:25:23 +0200

    Bytter fra jest til vitest
    remove all jest related files
    add vitest types
    update nx-dependency-checks rule to fix peerdeps

--- jest.preset.cjs
+++
@@ -1,7 +0,0 @@
-const nxPreset = require('@nx/jest/preset').default;
-
-module.exports = {
-  ...nxPreset,
-  transform: undefined,
-  coverageReporters: [...nxPreset.coverageReporters, 'lcov', 'json'],
-};
