b34f8a9f183c55a41c5860f3590575e28261823a
August Søberg-Klyver <august.soberg-klyver@skatteetaten.no>
2025-10-16 11:14:46 +0200

    Legger til tester for fallback-verdier som manglet tester

--- libs/ds-forms/src/Combobox/__tests__/defaults.test.ts
+++ libs/ds-forms/src/Combobox/__tests__/defaults.test.ts
@@ -3,6 +3,9 @@ import { dsI18n } from '@skatteetaten/ds-core-utils';
 import {
   getComboboxPlaceholderDefault,
   getComboboxLoadingMessageDefault,
+  getComboboxIsMultiSelectDefault,
+  getComboboxIsLoadingDefault,
+  getCombobboxMinSearchLengthDefault,
 } from '../defaults';
 
 describe('defaults', () => {
@@ -16,5 +19,17 @@ describe('defaults', () => {
     expect(getComboboxLoadingMessageDefault()).toBe(
       dsI18n.t('ds_forms:combobox.Loading')
     );
+  });
+
+  it('Når getComboboxIsMultiSelectDefault kalles, så returnerer den riktig verdi', () => {
+    expect(getComboboxIsMultiSelectDefault()).toBe(false);
+  });
+
+  it('Når getComboboxIsLoadingDefault kalles, så returnerer den riktig verdi', () => {
+    expect(getComboboxIsLoadingDefault()).toBe(false);
+  });
+
+  it('Når getCombobboxMinSearchLengthDefault kalles, så returnerer den riktig verdi', () => {
+    expect(getCombobboxMinSearchLengthDefault()).toBe(0);
   });
 });
