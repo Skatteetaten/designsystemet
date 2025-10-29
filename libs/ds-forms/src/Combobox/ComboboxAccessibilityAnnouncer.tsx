70b23031ad4bead3e33b7490a6f1a1e5c339cf0a
Hegge <annmagdalen.hegge@skatteetaten.no>
2025-10-29 14:26:24 +0100

    Remove srOnly text after three seconds

--- libs/ds-forms/src/Combobox/ComboboxAccessibilityAnnouncer.tsx
+++ libs/ds-forms/src/Combobox/ComboboxAccessibilityAnnouncer.tsx
@@ -1,4 +1,4 @@
-import { JSX, useRef, memo } from 'react';
+import { JSX, useRef, memo, useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { dsI18n } from '@skatteetaten/ds-core-utils';
@@ -37,8 +37,27 @@ const ComboboxAccessibilityAnnouncerComponent = ({
 }: ComboboxAccessibilityAnnouncerProps): JSX.Element => {
   const { t } = useTranslation('ds_forms', { i18n: dsI18n });
   const announcerRef = useRef<HTMLDivElement>(null);
+  const [message, setMessage] = useState<string>();
 
-  const message = getAnnouncementMessage(isOpen, displayOptions, searchTerm, t);
+  useEffect(() => {
+    const newMessage = getAnnouncementMessage(
+      isOpen,
+      displayOptions,
+      searchTerm,
+      t
+    );
+    setMessage(newMessage);
+
+    if (newMessage) {
+      const timeout = setTimeout(() => {
+        setMessage('');
+      }, 3000);
+      return (): void => {
+        clearTimeout(timeout);
+      };
+    }
+    return undefined;
+  }, [isOpen, displayOptions, searchTerm, t]);
 
   return (
     <div
