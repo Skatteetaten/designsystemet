6b77dc538eab8600fb9ff2e2aa503857ff479f4d
August Søberg-Klyver <august.soberg-klyver@skatteetaten.no>
2025-10-29 09:12:02 +0100

    refaktorerer PaginationList
    Erstatter magiske strenger med typesikre konstanter

--- libs/ds-navigation/src/PaginationList/utils.ts
+++ libs/ds-navigation/src/PaginationList/utils.ts
@@ -1,15 +1,27 @@
-export const canHaveElipsisEnd = (
+import { EllipsisType, PaginationItem } from '../Pagination/Pagination.types';
+
+export const ELLIPSIS = {
+  BEFORE: 'ellipsis-before',
+  AFTER: 'ellipsis-after',
+} as const;
+
+export const canHaveEllipsisEnd = (
   active: number,
   sibling: number,
   lastPage: number
 ): boolean => active + sibling + 1 < lastPage - 1;
 
-export const canHaveElipsisStart = (active: number, sibling: number): boolean =>
-  active - sibling - 1 > 2;
+export const canHaveEllipsisStart = (
+  active: number,
+  sibling: number
+): boolean => active - sibling - 1 > 2;
 
 export const getRange = (start: number, end: number): number[] =>
   Array.from({ length: end - start + 1 }, (_, i) => start + i);
 
-export const isElipsis = (paging: string | number): boolean => {
-  return paging?.toString().match(/^elips/) ? true : false;
+export const isEllipsis = (item: PaginationItem): item is EllipsisType => {
+  return (
+    typeof item === 'string' &&
+    (item === ELLIPSIS.BEFORE || item === ELLIPSIS.AFTER)
+  );
 };
