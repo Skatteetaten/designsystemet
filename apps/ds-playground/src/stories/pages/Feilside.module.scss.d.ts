060e27df0ff085c3bc5a33f32dbbbcb583044185
Jørgen Høivik Bye <jorgenhoivik.bye@skatteetaten.no>
2026-01-05 14:05:05 +0100

    FRONT-2201: Legg til eksempel for feilside

---
+++ apps/ds-playground/src/stories/pages/Feilside.module.scss.d.ts
@@ -0,0 +1,8 @@
+declare const classNames: {
+  readonly mainContent: 'mainContent';
+  readonly article: 'article';
+  readonly label: 'label';
+  readonly ingress: 'ingress';
+  readonly openClose: 'openClose';
+};
+export default classNames;
