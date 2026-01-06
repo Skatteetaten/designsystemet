f7ef68b57dfe869b490a153af8e799fd26c2a8e6
Jørgen Høivik Bye <jorgenhoivik.bye@skatteetaten.no>
2026-01-06 11:49:36 +0100

    fix link for topbannerinternal

--- libs/ds-layout/src/TopBannerInternal/TopBannerInternal.tsx
+++ libs/ds-layout/src/TopBannerInternal/TopBannerInternal.tsx
@@ -21,7 +21,7 @@ import styles from './TopBannerInternal.module.scss';
  * TopBannerInternal
  *
  * @see [Storybook](https://skatteetaten.github.io/designsystemet/?path=/docs/komponenter-topbannerinternal--docs) - Teknisk dokumentasjon
- * @see [Stil og tone](https://www.skatteetaten.no/stilogtone/designsystemet/komponenter/topbannerinternal/) - Brukerveiledning
+ * @see [Stil og tone](https://www.skatteetaten.no/stilogtone/designsystemet/komponenter/topbanner-internal/) - Brukerveiledning
  */
 export const TopBannerInternal = (({
   ref,
