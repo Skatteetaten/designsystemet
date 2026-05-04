70d25edb9f0d20570003c6d308f5926662309b68
Jørgen Høivik Bye <JorgenHoivik.Bye@skatteetaten.no>
2026-05-04 13:18:22 +0200

    FRONT-2201: Legg til feilside

---
+++ apps/ds-playground/src/stories/pages/Feilside.module.scss.d.ts
@@ -0,0 +1,9 @@
+declare const classNames: {
+  readonly mainContent: 'mainContent';
+  readonly article: 'article';
+  readonly label: 'label';
+  readonly ingress: 'ingress';
+  readonly link: 'link';
+  readonly openClose: 'openClose';
+};
+export default classNames;
