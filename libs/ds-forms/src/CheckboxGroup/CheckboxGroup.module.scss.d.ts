7ca3c9b7e07c0f582c1543d3de0d6b15eb00fe5f
Ann Magdalen <annmagdalen.hegge@skatteetaten.no>
2026-05-13 13:20:14 +0200

    Remove spacing last child CheckboxGroup

--- libs/ds-forms/src/CheckboxGroup/CheckboxGroup.module.scss.d.ts
+++ libs/ds-forms/src/CheckboxGroup/CheckboxGroup.module.scss.d.ts
@@ -1,6 +1,6 @@
 declare const classNames: {
   readonly errorMessage: 'errorMessage';
-  readonly contentContainer: 'contentContainer';
   readonly contentContainerSpacing: 'contentContainerSpacing';
+  readonly checkboxGroupContainer: 'checkboxGroupContainer';
 };
 export default classNames;
