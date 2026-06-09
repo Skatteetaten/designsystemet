0585e2b3e0164c6b1026f2a1005e1b6b26c866bf
Jørgen Høivik Bye <JorgenHoivik.Bye@skatteetaten.no>
2026-06-09 10:36:31 +0200

    remove a11y parameters

--- apps/ds-playground/src/stories/__tests__/RadioGroup.test.stories.tsx
+++ apps/ds-playground/src/stories/__tests__/RadioGroup.test.stories.tsx
@@ -185,9 +185,6 @@ export const WithCustomClassNames = {
   },
   parameters: {
     imageSnapshot: { disableSnapshot: true },
-    a11y: {
-      test: 'off',
-    },
   },
   play: async ({ canvasElement }): Promise<void> => {
     const canvas = within(canvasElement);
