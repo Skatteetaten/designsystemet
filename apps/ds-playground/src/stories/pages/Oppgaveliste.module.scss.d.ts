48127e3413e2e531484b448b5de6ef43555a647b
Ann Magdalen <annmagdalen.hegge@skatteetaten.no>
2026-06-15 14:47:32 +0200

    Remove hardcoded media width from css files

---
+++ apps/ds-playground/src/stories/pages/Oppgaveliste.module.scss.d.ts
@@ -0,0 +1,19 @@
+declare const classNames: {
+  readonly mainExternal: 'mainExternal';
+  readonly marginRightButton: 'marginRightButton';
+  readonly task: 'task';
+  readonly taskLink: 'taskLink';
+  readonly article: 'article';
+  readonly heading: 'heading';
+  readonly panel: 'panel';
+  readonly headingIcon: 'headingIcon';
+  readonly headingIcon_passive: 'headingIcon_passive';
+  readonly headingIcon_active: 'headingIcon_active';
+  readonly headingIconIcon: 'headingIconIcon';
+  readonly marginTopXl: 'marginTopXl';
+  readonly marginTopM: 'marginTopM';
+  readonly sendInButton: 'sendInButton';
+  readonly resetList: 'resetList';
+  readonly resetListElement: 'resetListElement';
+};
+export default classNames;
--- apps/ds-playground/src/stories/pages/RepeterendeFelter.stories.tsx
+++ apps/ds-playground/src/stories/pages/RepeterendeFelter.stories.tsx
@@ -6,7 +6,7 @@ import {
   formatOrganisationNumber,
   useMediaQuery,
 } from '@skatteetaten/ds-core-utils';
-import { TextField } from '@skatteetaten/ds-forms';
+import { TextField, FileUploader } from '@skatteetaten/ds-forms';
 import {
   AddSVGpath,
   ArrowBackSVGpath,
@@ -18,8 +18,8 @@ import {
 import { type Business, Modal } from '@skatteetaten/ds-overlays';
 import { Heading, Paragraph } from '@skatteetaten/ds-typography';
 
-import styles from './RepeterendeFelter.module.css';
-import stylesAsString from './RepeterendeFelter.module.css?raw';
+import styles from './RepeterendeFelter.module.scss';
+import stylesAsString from './RepeterendeFelter.module.scss?raw';
 import { includeStylesTransform } from '../../../.storybook/helpers';
 import { withPageLayout } from '../../../.storybook/pagelayout-decorator';
 
