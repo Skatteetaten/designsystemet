551d5ec3c1ef8c99341041214b307a51ee4062dc
Jørgen Høivik Bye <JorgenHoivik.Bye@skatteetaten.no>
2026-06-24 13:40:51 +0200

    fix test

--- apps/ds-playground/src/stories/__tests__/AccordionItem.test.stories.tsx
+++ apps/ds-playground/src/stories/__tests__/AccordionItem.test.stories.tsx
@@ -133,7 +133,8 @@ export const WithAttributes = {
   play: async ({ canvasElement }): Promise<void> => {
     const canvas = within(canvasElement);
     const button = canvas.getByRole('button');
-    await expect(button).toHaveClass('dummyClassname');
+    const container = button.parentElement?.parentElement;
+    await expect(container).toHaveClass('dummyClassname');
     await expect(button).toHaveAttribute('id', elementId);
     await expect(button).toHaveAttribute('lang', 'nb');
     await expect(button).toHaveAttribute('data-testid', '123ID');
