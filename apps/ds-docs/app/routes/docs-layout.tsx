d22fa55c3daa94896d22e39ec831b19518f12922
Eivind Løland-Andersen <eivind.loland-andersen@skatteetaten.no>
2026-08-25 15:24:13 +0200

    Legge til footer

--- apps/ds-docs/app/routes/docs-layout.tsx
+++ apps/ds-docs/app/routes/docs-layout.tsx
@@ -1,7 +1,8 @@
 import { JSX } from 'react';
 import { Outlet } from 'react-router';
 
-import { TopBannerExternal } from '@skatteetaten/ds-layout';
+import { Footer, TopBannerExternal } from '@skatteetaten/ds-layout';
+import { Paragraph } from '@skatteetaten/ds-typography';
 
 import { PageLayout } from '../components/page-layout';
 
@@ -12,6 +13,30 @@ export default function DocsLayout(): JSX.Element {
       <PageLayout>
         <Outlet />
       </PageLayout>
+      <Footer
+        titleSecondColumn={'Trenger du hjelp?'}
+        secondColumn={
+          <>
+            <Paragraph hasSpacing>
+              {
+                'Hvis du trenger hjelp eller råd er du velkommen til å ta kontakt. Vi kan hjelpe med konkrete problemstillinger og avklaringer.'
+              }
+            </Paragraph>
+            <Paragraph>
+              {
+                'Ta kontakt med kommunikasjonsavdelingen i Skattedirektoratet hvis du lurer på noe.'
+              }
+            </Paragraph>
+          </>
+        }
+        thirdColumn={
+          <Paragraph>
+            {
+              'Målgruppen for disse sidene er Skatteetatens ansatte og våre samarbeidspartnere. Men vi vil gjerne at andre også kan se hvordan vi jobber. Bruk det, bli inspirert av det og gi tilbakemelding hvis du mener noe.'
+            }
+          </Paragraph>
+        }
+      />
     </>
   );
 }
