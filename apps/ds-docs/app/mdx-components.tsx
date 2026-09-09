815d55f95a564c38aaf5af8fd818cb729221bed6
Ann Magdalen <annmagdalen.hegge@skatteetaten.no>
2026-09-09 14:54:43 +0200

    Fix headings endringslogg

--- apps/ds-docs/app/mdx-components.tsx
+++ apps/ds-docs/app/mdx-components.tsx
@@ -93,7 +93,7 @@ const components = {
     children?: ReactNode;
     id?: string;
   }): JSX.Element => (
-    <Heading as={'h3'} level={3} id={id} hasSpacing={false}>
+    <Heading as={'h3'} id={id} hasSpacing={false}>
       {children}
     </Heading>
   ),
@@ -104,7 +104,29 @@ const components = {
     children?: ReactNode;
     id?: string;
   }): JSX.Element => (
-    <Heading as={'h4'} level={4} id={id} hasSpacing={false}>
+    <Heading as={'h4'} id={id} hasSpacing={false}>
+      {children}
+    </Heading>
+  ),
+  h4: ({
+    children,
+    id,
+  }: {
+    children?: ReactNode;
+    id?: string;
+  }): JSX.Element => (
+    <Heading as={'h5'} id={id} hasSpacing={false}>
+      {children}
+    </Heading>
+  ),
+  h5: ({
+    children,
+    id,
+  }: {
+    children?: ReactNode;
+    id?: string;
+  }): JSX.Element => (
+    <Heading as={'h6'} id={id} hasSpacing={false}>
       {children}
     </Heading>
   ),
